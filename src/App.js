c9849d9e207ccc706a4ea7c5b49922d1c863768f
Mosiur-Rahman-1 <masiurreheman01@gmail.com>
2020-11-15 18:59:32 +0600

    adding the contact componant to the team-code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './Componants/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Contact></Contact>
     </div>
   );
 }
